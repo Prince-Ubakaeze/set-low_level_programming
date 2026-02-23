dbca0d1dedab2d3b64ac48c4c868044c96fe3694
prince <ubakaezeprince@gmaail.com>
2026-02-23 22:12:17 +0100

    Thank God

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Descriptions: prints a string using the printf fundtion
+*
+* return always 0 (success)
+*/
+int main(void)
+{
+printf ("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
