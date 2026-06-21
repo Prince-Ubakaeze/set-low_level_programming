89bb60e240141fae30d6b6e293c59b95754cbf47
prince <ubakaezeprince@gmail.com>
2026-06-21 20:32:41 +0100

    Clean untracked compiled binary artifacts from repository

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -7,11 +7,6 @@
 
 /**
  * struct hash_node_s - Node of a hash table
- *
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
  */
 typedef struct hash_node_s
 {
@@ -22,11 +17,6 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
- *
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
  */
 typedef struct hash_table_s
 {
@@ -44,14 +34,6 @@ void hash_table_delete(hash_table_t *ht);
 
 /* --- Sorted Hash Table Data Structures --- */
 
-/**
- * struct shash_node_s - Node of a sorted hash table
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- * @sprev: A pointer to the previous element of the sorted linked list
- * @snext: A pointer to the next element of the sorted linked list
- */
 typedef struct shash_node_s
 {
 	char *key;
@@ -61,13 +43,6 @@ typedef struct shash_node_s
 	struct shash_node_s *snext;
 } shash_node_t;
 
-/**
- * struct shash_table_s - Sorted hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
- * @shead: A pointer to the first element of the sorted linked list
- * @stail: A pointer to the last element of the sorted linked list
- */
 typedef struct shash_table_s
 {
 	unsigned long int size;
